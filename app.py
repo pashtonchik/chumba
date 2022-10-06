e99d376af1aa269843fcf308623331837fbe79e8
pashtonchik <pashka191@yandex.ru>
2022-10-06 17:03:20 +0300

    mbmb

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.binary_location = "/home/pashton/Downloads/google-chrome-stable_current_amd64.deb"
-driver = webdriver.Chrome(executable_path='/home/pashton/Desktop/Projects/chumba/chromedriver')
+options = webdriver.ChromeOptions()
+options.add_argument("--allow-profiles-outside-user-dir")
+options.add_argument(r"user-data-dir=/home/pashton/.config/google-chrome")
+options.add_argument('enable-profile-shortcut-manager')
+options.add_argument('--profile-directory=Profile 1')
+# options.binary_location = "/home/pashton/Downloads/google-chrome-stable_current_amd64.deb"
+driver = webdriver.Chrome(
+    chrome_options=options,
+    executable_path='/home/pashton/Desktop/Projects/chumba/chromedriver'
+)
 
 url = 'https://vk.com/'
 
 driver.get(url=url)
-driver.close()
-driver.quit()
+# driver.close()
+# driver.quit()
 # /home/pashton/Downloads/google-chrome-stable_current_amd64.deb
